c43282c987fcc48b4fa8e97287847cf79f9e540a
aarchie-r <aarchieiitk@gmail.com>
2023-03-15 01:29:15 +0530

    method-invoke-revisited

--- input.java
+++ input.java
@@ -1,103 +1,106 @@
-public static class Fibonacci { //1
-    // String x = "xyz";
-    // String memo = "fir se danti"+"jjs";
-    // char uu = 'o';
-    char zu[][] = {{'t'}};
-    // float ff,ff1;
-    // String name[];
+// public static class Fibonacci { //1
+//     // String x = "xyz";
+//     // String memo = "fir se danti"+"jjs";
+//     // char uu = 'o';
+//     char zu[][] = {{'t'}};
+//     // float ff,ff1;
+//     // String name[];
 
-    // public Fibonacci(int n) {
-    //     // this.name[0]=9;
-    //     int tui;
-    //     // memo[2 = 5;
-    //     // name[2]=4;
-    //     // memo = new int[n + 1];
-    //     // memo[1][2][3]=5;
-    // }
+//     // public Fibonacci(int n) {
+//     //     // this.name[0]=9;
+//     //     int tui;
+//     //     // memo[2 = 5;
+//     //     // name[2]=4;
+//     //     // memo = new int[n + 1];
+//     //     // memo[1][2][3]=5;
+//     // }
 
-    // public Fibonacci(int n) {
-    //     this.name[0]=9;
-    //     int tui;
-    //     // memo[2 = 5;
-    //     name[2]=4;
-    //     // memo = new int[n + 1];
-    //     // memo[1][2][3]=5;
-    // }
+//     // public Fibonacci(int n) {
+//     //     this.name[0]=9;
+//     //     int tui;
+//     //     // memo[2 = 5;
+//     //     name[2]=4;
+//     //     // memo = new int[n + 1];
+//     //     // memo[1][2][3]=5;
+//     // }
 
-    {
-        int who;
-        char love;
-        final class You{
+//     {
+//         int who;
+//         char love;
+//         final class You{
 
-        }
-    }
+//         }
+//     }
 
-    static {
-        int me;
-        String dont;
-        float love;
-        final class You{
+//     static {
+//         int me;
+//         String dont;
+//         float love;
+//         final class You{
             
-        }
+//         }
 
-    }
+//     }
 
-    {
-        this.fib(1,3);
-        // new Fibonacci (2)= 3;
-    }
-}
-class Myclass{
-    // public int z=1;
-    // public class Onemore{
-    //     public static void fatak(char z){
+//     {
+//         this.fib(1,3);
+//         // new Fibonacci (2)= 3;
+//     }
+// }
+// class Myclass{
+//     // public int z=1;
+//     // public class Onemore{
+//     //     public static void fatak(char z){
 
-    //     }
-    // }
+//     //     }
+//     // }
 
-    // public Myclass(int n) {
-    //     memo11 = new int[n + 1];
-    // }
+//     // public Myclass(int n) {
+//     //     memo11 = new int[n + 1];
+//     // }
 
-    private int fib11(int n, int z) {
-        // if (n == 0 || n == 1) {
-        //     return n;
-        // }
+//     private int fib11(int n, int z) {
+//         // if (n == 0 || n == 1) {
+//         //     return n;
+//         // }
     
 
-        // if (memo[n] != 0) {
-        //     return memo[n];
-        // }
+//         // if (memo[n] != 0) {
+//         //     return memo[n];
+//         // }
 
-        int result;
-        result= 66;
-        if(result==0){
-            int s=8;
-            return 1;
-        }
-        for(int i=0;i<10;i++){
-            result=0;
-        }
-        // fib(n - 1) + fib(n - 2);
-        // memo[n] = result;
+//         int result;
+//         result= 66;
+//         if(result==0){
+//             int s=8;
+//             return 1;
+//         }
+//         for(int i=0;i<10;i++){
+//             result=0;
+//         }
+//         // fib(n - 1) + fib(n - 2);
+//         // memo[n] = result;
 
 
 
         
 
-        return 0;
-    }
+//         return 0;
+//     }
 
 
     
 
-}
+// }
 
-public class MyClass {
-    private int num;
+public class Calculator {
+  public int add(int x, int y) {
+    return x + y;
+  }
 
-    public static void main(String[] args) {
-        MyClass obj = new MyClass();
-        // obj.num = 5; // no error
-    }
+  public static void main(String[] args) {
+    Calculator calculator = new Calculator();
+    int sum = add(3, 'h');
+    System.out.println("The sum of 3 and 5 is " + sum);
+  }
 }
