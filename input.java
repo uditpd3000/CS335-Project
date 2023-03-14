d3d89704b6d94c7eed19334209082fe1a1cc6976
aarchie-r <aarchieiitk@gmail.com>
2023-03-14 23:17:53 +0530

    input.java

--- input.java
+++ input.java
@@ -1,19 +1,19 @@
-public static class Fibonacci { //1
-    // String x = "xyz";
-    // String memo = "fir se danti"+"jjs";
-    // char uu = 'o';
-    char zu[][] = {{'t'}};
-    // float ff,ff1;
-    // String name[];
+// public static class Fibonacci { //1
+//     // String x = "xyz";
+//     // String memo = "fir se danti"+"jjs";
+//     // char uu = 'o';
+//     char zu[][] = {{'t'}};
+//     // float ff,ff1;
+//     // String name[];
 
-    public Fibonacci(int n) {
-        // this.name[0]=9;
-        int tui;
-        // memo[2 = 5;
-        // name[2]=4;
-        // memo = new int[n + 1];
-        // memo[1][2][3]=5;
-    }
+//     public Fibonacci(int n) {
+//         // this.name[0]=9;
+//         int tui;
+//         // memo[2 = 5;
+//         // name[2]=4;
+//         // memo = new int[n + 1];
+//         // memo[1][2][3]=5;
+//     }
 
 //     public Fibonacci(int n) {
 //         this.name[0]=9;
@@ -62,49 +62,74 @@ public static class Fibonacci { //1
 //         this.fib(1,3);
 //         // new Fibonacci (2)= 3;
 //     }
-}
-class Myclass{
-    // public int z=1;
-    // public class Onemore{
-    //     public static void fatak(char z){
+// }
+// class Myclass{
+//     // public int z=1;
+//     // public class Onemore{
+//     //     public static void fatak(char z){
 
-    //     }
-    // }
+//     //     }
+//     // }
 
-    // public Myclass(int n) {
-    //     memo11 = new int[n + 1];
-    // }
+//     // public Myclass(int n) {
+//     //     memo11 = new int[n + 1];
+//     // }
 
-    private int fib11(int n, int z) {
-        // if (n == 0 || n == 1) {
-        //     return n;
-        // }
+//     private int fib11(int n, int z) {
+//         // if (n == 0 || n == 1) {
+//         //     return n;
+//         // }
     
 
-        // if (memo[n] != 0) {
-        //     return memo[n];
-        // }
+//         // if (memo[n] != 0) {
+//         //     return memo[n];
+//         // }
 
-        int result;
-        result= 66;
-        if(result==0){
-            int s=8;
-            return 1;
-        }
-        for(int i=0;i<10;i++){
-            result=0;
-        }
-        // fib(n - 1) + fib(n - 2);
-        // memo[n] = result;
+//         int result;
+//         result= 66;
+//         if(result==0){
+//             int s=8;
+//             return 1;
+//         }
+//         for(int i=0;i<10;i++){
+//             result=0;
+//         }
+//         // fib(n - 1) + fib(n - 2);
+//         // memo[n] = result;
 
 
 
         
 
-        return 0;
-    }
+//         return 0;
+//     }
 
 
     
 
+// }
+
+public class TypeChecker {
+    public static void main(String[] args) {
+        int x = 5;
+        String y = "hello";
+        
+        // Check types
+        if (x instanceof String) {
+            System.out.println("x is an Integer");
+        } else {
+            System.out.println("x is not an Integer");
+        }
+        
+        if (y instanceof String) {
+            System.out.println("y is a String");
+        } else {
+            System.out.println("y is not a String");
+        }
+        
+        // Try to assign incompatible types
+        // This will result in a compilation error
+        int z = "world";
+        String w = 10;
+    }
 }
