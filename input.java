75952ba696f1424d55249c402e35dde65c72b181
aarchie-r <aarchieiitk@gmail.com>
2023-03-23 22:21:22 +0530

    enhanced-for

--- input.java
+++ input.java
@@ -315,30 +315,30 @@
 public class AddMatrices {
   // data = new int[] {1,2,3};
   public static void main(String[] args) {
-      int rows = 4, columns = 3;
-      int firstMatrix[][] = { {2, 3, 4}, {5, 2, 3} };
-      int secondMatrix[][] = { {-4, 5, 3}, {5, 6, 3} };
+      // int rows = 4, columns = 3;
+      // int firstMatrix[][] = { {2, 3, 4}, {5, 2, 3} };
+      // int secondMatrix[][] = { {-4, 5, 3}, {5, 6, 3} };
       
-      // System.out.println("k");
+      // // System.out.println("k");
 
-      // Adding Two matrices
-      int sum[][] = new int[rows][columns];
-      // int sum1[][] = new int[][]{{1,2},{2,2}};
-      for(int i = 0; i < rows; i++) {
-          for (int j = 0; j < columns; j++) {
-            // int sum[][] = new int[rows][columns];
-              sum[i][j] = firstMatrix[i][j] + secondMatrix[i][j];
-              int sum1[][] = new int[][]{{1,2},{2,2}};
-          }
-      }
+      // // Adding Two matrices
+      int sum[] = new int[4];
+      int sum1[][] = new int[][]{{1,2},{2,2}};
+      // for(int i = 0; i < rows; i++) {
+      //     for (int j = 0; j < columns; j++) {
+      //       // int sum[][] = new int[rows][columns];
+      //         // sum[i][j] = firstMatrix[i][j] + secondMatrix[i][j];
+      //         // int sum1[][] = new int[][]{{1,2},{2,2}};
+      //     }
+      // }
 
       // // Displaying the result
       System.out.println("Sum of two matrices is: ");
-      // for(int row[] : sum1) {
-      //     for (int column : row) {
-      //         System.out.println("    ");
-      //     }
-      //     System.out.println("end");
-      // }
+      for(int row[] : sum1) {
+          for (int column : sum) {
+              System.out.println("    ");
+          }
+          System.out.println("end");
+      }
   }
 }
