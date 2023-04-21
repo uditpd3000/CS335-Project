7cc53bbac9da9a250fccdeeaff20396bf3a2233f
Udit <uditpd3000@gmail.com>
2023-04-22 00:09:32 +0530

    Done and dustedgit status

--- input.java
+++ input.java
@@ -1,19 +1,35 @@
-public class Rectangle {
-    private int length;
-    private int width;
+public class SieveOfEratosthenesDemo {
 
-    public Rectangle(int length, int width) {
-        this.length = length;
-        this.width = width;
+  public static int sieveOfEratosthenes(int n) {
+    int primes[] = new int[100];
+    for (int i = 2; i * i <= n; i++) {
+      primes[i] = 1;
     }
 
-    public int area() {
-        return this.length * this.width;
-    }
+    primes[0] = 0;
+    primes[1] = 0;
 
-    public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(5, 10);
-        int myarea = rectangle.area();
-        System.out.println(myarea);
+    if (n == 2)
+      return 1;
+
+    for (int i = 2; i * i <= n; i++) {
+      if (primes[i] == 1) {
+        if (n % i == 0)
+          return 0;
+        for (int j = i * i; j <= n; j += i) {
+          primes[j] = 0;
+        }
+      }
     }
+    return 1;
+  }
+
+  public static void main(String[] args) {
+    int n = 7;
+    int x = 2;
+    x = sieveOfEratosthenes(n);
+    if (x == 1) {
+      System.out.println(n);
+    }
+  }
 }
